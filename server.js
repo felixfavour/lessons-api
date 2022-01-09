090a9b6832cf87562bbe0bf2eaf71fd473b3fcaf
Favour Felix <felixfavour0@gmail.com>
2022-01-10 00:50:09 +0400

    added route for returning lessons array in json

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const lessons = require('./static/lessons')
 const app = express()
 const port = 3000
+
+// Routes
+app.get('/lessons', function(request, response) {
+  return response.json(lessons)
+})
 
 app.listen(port)
 console.log(`App is currently hosted on port: ${port}`)
