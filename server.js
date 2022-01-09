bd991910648e5e03195691988b454908501c1377
Favour Felix <felixfavour0@gmail.com>
2022-01-10 00:47:03 +0400

    added lessons file

--- server.js
+++ server.js
@@ -2,6 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
 app.listen(port)
 console.log(`App is currently hosted on port: ${port}`)
---
+++ static/lessons.js
@@ -0,0 +1,8 @@
+const lessons = [
+  { 'topic': 'math', 'location': 'London', 'price': 100 },
+  { 'topic': 'math', 'location': 'Liverpool', 'price': 80 },
+  { 'topic': 'math', 'location': 'Oxford', 'price': 90 },
+  { 'topic': 'math', 'location': 'Bristol', 'price': 120 }
+]
+
+module.exports = lessons
